2b17cc618399a20059c64861eed37ef2e416f973
fm16191 <56755856+fm16191@users.noreply.github.com>
2023-02-23 12:17:08 +0100

    Add run script

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Must be evaled ?
+
+[[ $0 != "$BASH_SOURCE" ]] || { echo -e "Script must be source, please \". $0\" instead."; exit 0; }
+
+free_image_path="../FreeImage"
+
+if [ -d $free_image_path ]; then
+    echo -e "Must install the FreeImage lib.\nReturn to continue..."
+    read
+    tar -xf FreeImage3180.tar.gz
+    cd FreeImage && make && make install
+fi
+
+list=$(module list)
+(echo "$list" | grep cuda > /dev/null) || (echo "Loading cuda ..." && module load cuda)
+(echo "$list" | grep "gcc/10.2.0" > /dev/null) || (echo "Loading gcc@10.2.0 ..." && module load gcc/10.2.0)
+
+make && ./modif_img.exe
